064980da2bd8c816368722da9bcc49e28f28d6f2
cryptolemming <aliayoub22@gmail.com>
2015-07-25 17:58:38 -0400

    created view

---
+++ components/Locations.jsx
@@ -0,0 +1,33 @@
+var React = require('react');
+var LocationStore = require('../stores/LocationStore');
+
+var Locations = React.createClass({
+	getInitialState() {
+		return LocationStore.getState();
+	},
+
+	componentDidMount() {
+		LocationStore.listen(this.onChange);
+	},
+
+	componentWillUnmount() {
+		LocationStore.unlisten(this.onChange);
+	},
+
+	onChange(state) {
+		this.setState(state);
+	},
+
+	render() {
+		return (
+			<ul>
+				{this.state.locations.map(locations => {
+					return (
+						<li>{location.name}</li>
+					),
+				});
+			</ul>
+		),
+	};
+});
+
